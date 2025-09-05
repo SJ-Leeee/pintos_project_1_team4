2c12471108717b4f9a87162b89acd5b61e96a5e9
leeseungjun <cjstkrak@gmail.com>
2025-09-05 15:08:06 +0900

    format: Microsoft 스타일로 파일포멧

--- seungjun/pintos/tests/userprog/child-simple.c
+++ seungjun/pintos/tests/userprog/child-simple.c
@@ -2,14 +2,13 @@
    wait-twice tests.
    Just prints a single message and terminates. */
 
-#include <stdio.h>
 #include "tests/lib.h"
+#include <stdio.h>
 
-int
-main (void) 
+int main(void)
 {
-  test_name = "child-simple";
+    test_name = "child-simple";
 
-  msg ("run");
-  return 81;
+    msg("run");
+    return 81;
 }
